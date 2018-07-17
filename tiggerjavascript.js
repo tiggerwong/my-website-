5dfc7197c51d4824f0d4ecd55a8f0b373244f3fa
tiggerwong <41347758+tiggerwong@users.noreply.github.com>
2018-07-17 15:12:45 -0700

    Add files via upload

---
+++ tiggerjavascript.js
@@ -0,0 +1,10 @@
+function changeHeading() {
+
+  document.getElementById("heading").innerHTML = "It's nice to meet you";
+}
+
+
+
+function showtiggerrollingpic() {
+    document.getElementById("buttonclicked").innerHTML = "<img src=\"tiggerrolling.png\" height=\"500\" width=\"500\">"
+}
